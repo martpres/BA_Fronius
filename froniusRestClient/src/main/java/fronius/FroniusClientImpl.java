96ee224a7e080dc4d085c556a446f702bcadb319
Martin Pressl <99990993+martpres@users.noreply.github.com>
2024-04-04 19:42:06 +0200

    finished rest client

--- froniusRestClient/src/main/java/fronius/FroniusClientImpl.java
+++ froniusRestClient/src/main/java/fronius/FroniusClientImpl.java
@@ -1,16 +1,26 @@
 package fronius;
 
 import dto.CurrentAcDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import service.FroniusClient;
 
 @Service
 public class FroniusClientImpl implements FroniusClient {
-    RestClient restClient = RestClient.create();
+    private final RestClient restClient;
+    private final String froniusUrl;
+
+    public FroniusClientImpl(@Value("${app.restclient.fronius-url}") String froniusUrl) {
+        this.restClient = RestClient.create();
+        this.froniusUrl = froniusUrl;
+    }
 
     @Override
     public CurrentAcDto currentAcEndpoint() {
-        return null;
+        return restClient.get()
+                .uri(froniusUrl + "GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0")
+                .retrieve()
+                .body(CurrentAcDto.class);
     }
 }

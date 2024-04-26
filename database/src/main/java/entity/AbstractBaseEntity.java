f9198006baab30867e8a48701c24771dea04f0ba
Martin Pressl <99990993+martpres@users.noreply.github.com>
2024-04-26 18:27:07 +0200

    added timezone to database

--- database/src/main/java/entity/AbstractBaseEntity.java
+++ database/src/main/java/entity/AbstractBaseEntity.java
@@ -2,20 +2,21 @@ package entity;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @MappedSuperclass
 public abstract class AbstractBaseEntity implements Serializable {
 
     @Id
-    @Column(name = "primary_timestamp", updatable = false, nullable = false)
+    @Column(name = "primary_timestamp", updatable = false, nullable = false, columnDefinition = "timestamp with time zone")
     private ZonedDateTime timestamp;
 
     public ZonedDateTime getTimestamp() {
-        return timestamp;
+        return timestamp.withZoneSameInstant(ZoneId.of("UTC"));
     }
 
     public void setTimestamp(ZonedDateTime timestamp) {
-        this.timestamp = timestamp;
+        this.timestamp = timestamp.withZoneSameInstant(ZoneId.of("UTC"));
     }
 }
